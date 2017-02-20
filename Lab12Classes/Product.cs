b4cd78a6ea450aee4f2e2334d9aba57153ad4187
patricksheridansd <patrick_sheridan@outlook.com>
2017-02-20 12:07:01 +0000

    c# product class
    Built a product class with a constructor to initialise variables and
    different methods to update the instances of the product class

---
+++ Lab12Classes/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Lab12Classes
+{
+    class Product
+    {
+        int productCode;
+        string productName;
+        double sellingPrice;
+        double costPrice;
+        int inStock;
+        double totalProfit;
+        double profit;
+        const double VAT = 1.20;
+
+        //Constructor or initialiser
+        public Product(int pCode, string pName, double pSellPrice, double pCostPrice)
+        {
+            productCode = pCode;
+            productName = pName;
+            sellingPrice = pSellPrice;
+            costPrice = pCostPrice;
+        }
+
+        //Method to update stock
+        public void RenewStockLevel(int stock)
+        {
+            inStock = inStock + stock;
+        }
+
+
+        // Method for sale of product
+        public bool Sell(int numSold)
+        {
+
+            if(numSold > inStock )
+            {
+                Console.WriteLine("Not enough in stock");
+                return false;
+            }
+            else
+            {
+                profit =  (sellingPrice - costPrice)* numSold;
+                totalProfit = totalProfit + profit;
+                profit = 0;
+                inStock = inStock - numSold;
+                return true;
+            }
+        }
+
+        //method to display current stock
+        public int GetStock()
+        {
+            Console.WriteLine("Units of product left = " + inStock);
+            return inStock;   
+        }
+
+        //method to get total profit
+        public double GetAccumulatedProfit()
+        {
+            Console.WriteLine("Total profit is : " + totalProfit);
+            return totalProfit;
+        }
+
+    }
+}
---
+++ Lab12Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Lab12Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int purchaced;
+
+            Product newProduct = new Product(1234, "toy", 10, 7);
+            newProduct.RenewStockLevel(100);
+            newProduct.GetStock();
+            Console.WriteLine("How many would you like to buy: ");
+            purchaced = Int32.Parse(Console.ReadLine());
+            newProduct.Sell(purchaced);
+            newProduct.GetAccumulatedProfit();
+
+
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Would you like to buy more?");
+                newProduct.GetStock();
+                Console.WriteLine("Enter ammount to purchace or Input 0 to EXIT : ");
+                purchaced = Int32.Parse(Console.ReadLine());
+                newProduct.Sell(purchaced);
+                newProduct.GetAccumulatedProfit();
+
+            } while (purchaced != 0);
+
+
+
+        }
+    }
+}
